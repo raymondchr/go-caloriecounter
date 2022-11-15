1702e6b1ac45c3265ad535ded1820bc089858aa5
raymondchr <raycuffed@gmail.com>
2022-11-15 10:51:16 +0700

    first commit

---
+++ server/routes/entries.go
@@ -0,0 +1,78 @@
+package routes
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var entryCollection *mongo.Collection = openCollection(Client, "calories")
+
+func AddEntry(c *gin.Context) {
+
+}
+
+func GetEntries(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	var entries []bson.M
+	cursor, err := entryCollection.Find(ctx, bson.M{})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		fmt.Println(err)
+		return
+	}
+
+	if err := cursor.All(ctx, &entries); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+	fmt.Println(entries)
+	c.JSON(http.StatusOK, entries)
+}
+
+func GetEntryByIngredient(c *gin.Context) {
+
+}
+
+func GetEntryById(c *gin.Context) {
+
+}
+
+func UpdateEntry(c *gin.Context) {
+
+}
+
+func UpdateIngredient(c *gin.Context) {
+
+}
+
+func DeleteEntry(c *gin.Context) {
+	Id := c.Params.ByName("id")
+	docID, _ := primitive.ObjectIDFromHex(Id)
+
+	//context.WithTimeout prevents the DB to run all the time, saving resources
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	result, err := entryCollection.DeleteOne(ctx, bson.M{"_id": docID})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+
+	c.JSON(http.StatusOK, result.DeletedCount)
+
+}
